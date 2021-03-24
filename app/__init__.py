6109c33b691ae4da7bccf7aee35de9c5979cdc5e
harry <harrisongitau3@gmail.com>
2021-03-24 11:50:27 +0300

    Enable user login

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,21 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+mail = Mail(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from app import routes, models, errors
