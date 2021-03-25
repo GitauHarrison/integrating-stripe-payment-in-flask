c214d4c7c865bfcac2f9b544c907e8539de07567
harry <harrisongitau3@gmail.com>
2021-03-25 16:04:57 +0300

    Remove redundant code

--- app/email.py
+++ app/email.py
@@ -1,6 +1,5 @@
 from flask_mail import Message
 from app import mail
-from flask import render_template
 from threading import Thread
 from flask import current_app
 
@@ -17,13 +16,3 @@ def send_email(subject, sender, recipients, text_body, html_body):
     Thread(target=send_async_email,
            args=(current_app._get_current_object(), msg)).start()
 
-
-def send_password_reset_email(user):
-    token = user.get_reset_password_token()
-    send_email('[Stripe Flask] Reset Your Password',
-               sender=current_app.config['ADMINS'][0],
-               recipients=[user.email],
-               text_body=render_template('email/reset_password.txt',
-                                         user=user, token=token),
-               html_body=render_template('email/reset_password.html',
-                                         user=user, token=token))
