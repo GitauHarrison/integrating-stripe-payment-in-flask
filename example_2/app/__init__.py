adb8e3b8d2fe7246f00a8a0a3607c8c43d44307d
harry <harrisongitau3@gmail.com>
2021-12-30 21:52:10 +0300

    Dev: Restructure repo

--- example_2/app/__init__.py
+++ example_2/app/__init__.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from config import Config
 import logging
-from logging.handlers import SMTPHandler, RotatingFileHandler
+from logging.handlers import RotatingFileHandler
 import os
 
 app = Flask(__name__)
@@ -30,24 +30,25 @@ products = {
     }
 
 if not app.debug:
-        if app.config['LOG_TO_STDOUT']:
-            stream_handler = logging.StreamHandler()
-            stream_handler.setLevel(logging.INFO)
-            app.logger.addHandler(stream_handler)
-        else:
-            if not os.path.exists('logs'):
-                os.mkdir('logs')
-            file_handler = RotatingFileHandler('logs/stripe_flask.log',
-                                               maxBytes=10240,
-                                               backupCount=10
-                                               )
-            file_handler.setFormatter(logging.Formatter(
-                '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    if app.config['LOG_TO_STDOUT']:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler(
+            'logs/stripe_flask.log',
+            maxBytes=10240,
+            backupCount=10
+            )
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
             ))
-            file_handler.setLevel(logging.INFO)
-            app.logger.addHandler(file_handler)
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
 
-        app.logger.setLevel(logging.INFO)
-        app.logger.info('Stripe Flask Integration')
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Stripe Flask Integration')
 
 from app import routes
